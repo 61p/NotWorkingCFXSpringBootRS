353abefeed32339816d71f4442f77ac4afa89e22
hannahanen <patrikhansson17@hotmail.com>
2018-12-18 12:50:24 +0100

    init

---
+++ src/main/java/com/firstPackage/StarterPoint.java
@@ -0,0 +1,13 @@
+package com.firstPackage;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StarterPoint {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StarterPoint.class,args);
+    }
+}
