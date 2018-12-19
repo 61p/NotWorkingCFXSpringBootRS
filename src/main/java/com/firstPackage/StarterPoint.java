2ecf602113dfc7c4e34650311ed592daae32b2af
hannahanen <patrikhansson17@hotmail.com>
2018-12-19 09:32:57 +0100

    second init

--- src/main/java/com/firstPackage/StarterPoint.java
+++ src/main/java/com/firstPackage/StarterPoint.java
@@ -1,13 +1,14 @@
 package com.firstPackage;
 
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class StarterPoint {
 
+
     public static void main(String[] args) {
         SpringApplication.run(StarterPoint.class,args);
     }
+
 }
--- src/main/java/com/firstPackage/secondPackage/RestApi.java
+++ src/main/java/com/firstPackage/secondPackage/RestApi.java
@@ -1,19 +1,24 @@
 package com.firstPackage.secondPackage;
 
+import org.springframework.context.annotation.ImportResource;
+import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
 
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
 
 @Service
-@Path("/Hi")
+@Path("/hi")
 public class RestApi {
+
+    public RestApi(){}
 
     @GET
     @Produces(MediaType.TEXT_PLAIN)
-    public String helloAPI(){
-        return "API IS WORKING!";
+    public Response.ResponseBuilder helloAPI(){
+        return Response.ok("WOWO YEYE");
     }
 }
